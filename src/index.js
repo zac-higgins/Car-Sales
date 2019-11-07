a5ff9ebf031670c5232aded141f63ebf036a11c3
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-07 07:12:30 -0700

    added actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { reducer } from "./reducers";
+import { Provider } from 'react-redux';
 
+//styles
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
